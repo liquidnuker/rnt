d988499b1d1d8bc685f41896e8437cac42401d17
administrator <liquidnuker@users.noreply.github.com>
2018-01-17 10:46:57 -0800

    added echo-js import

--- src/index.js
+++ src/index.js
@@ -3,19 +3,38 @@
 // 
 // ======================================================/
 // import Blazy from "./js/vendor/bLazy.min.js"; // ok
-import Blazy from "blazy"; // ok
+// import Blazy from "blazy"; // ok
 
-let bLazy = new Blazy({
-  success: function (ele) {
-    // Image has loaded
-    // Do your business here
-    console.log(ele.getAttribute("src"));
-  },
-  error: function (ele, msg) {
-    if (msg === 'missing') {
-      // Data-src is missing
-    } else if (msg === 'invalid') {
-      // Data-src is invalid
-    }
-  }
-});
+// let bLazy = new Blazy({
+//   success: function (ele) {
+//     // Image has loaded
+//     // Do your business here
+//     console.log(ele.getAttribute("src"));
+//   },
+//   error: function (ele, msg) {
+//     if (msg === 'missing') {
+//       // Data-src is missing
+//     } else if (msg === 'invalid') {
+//       // Data-src is invalid
+//     }
+//   }
+// });
+
+// 
+// ======================================================/
+// import echo from "echo-js"; // ok
+
+// echo.init({
+//     offset: 100,
+//     throttle: 250,
+//     unload: false,
+//     callback: function (element, op) {
+//       if(op === 'load') {
+//         console.log(op);
+//         // element.classList.add('loaded');
+//       } else {
+//         console.log(op);
+//         // element.classList.remove('loaded');
+//       }
+//     }
+//   });

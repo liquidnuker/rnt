16cfefc32426b9c4541d20fae921462c629c465a
administrator <liquidnuker@users.noreply.github.com>
2018-01-19 09:48:47 -0800

    added options

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 // ======================================================/
 window.addEventListener("load", function () {
   let images = document.getElementsByTagName("img");
-  console.log("img loaded");
+  console.log("vanilla lazyloaded");
   for (let i = 0, l = images.length; i < l; i++) {
     if (images[i].getAttribute("data-src")) {
       images[i].setAttribute("src", images[i].getAttribute("data-src"));
@@ -18,16 +18,34 @@ window.addEventListener("load", function () {
 import Blazy from "blazy"; // ok
 
 let bLazy = new Blazy({
+  // src: 'data-blazy', // Default is data-src
+
+  // selector
+  // selector: 'img', // all images
+
+  // images inside container
+  // container: '#scrolling-container', // Default is window
+
+  // offset: 100, // Loads images 100px before they're visible
+
+  // breakpoints: [{
+  //   width: 420, // max-width
+  //   src: 'data-src-small'
+  // },
+  // {
+  //   width: 768, // max-width
+  //   src: 'data-src-medium'
+  // }],
+  
   success: function (ele) {
-    // Image has loaded
-    // Do your business here
+    console.log("bLazy image loaded");
     console.log(ele.getAttribute("src"));
   },
   error: function (ele, msg) {
     if (msg === "missing") {
-      // Data-src is missing
+      console.log("bLazy data-src is missing");
     } else if (msg === "invalid") {
-      // Data-src is invalid
+      console.log("bLazy data-src is invalid");
     }
   }
 });
@@ -42,7 +60,8 @@ echo.init({
   unload: false,
   callback: function (element, op) {
     if (op === "load") {
-      console.log(op);
+      console.log("echo-js loaded");
+      console.log(`echo-js ${element} ${op}`);
       // element.classList.add("loaded");
     } else {
       console.log(op);
